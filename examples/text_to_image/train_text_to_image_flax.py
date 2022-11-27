80028afbd6abe47e973077c542e169a01074fded
lk-wq <55562097+lk-wq@users.noreply.github.com>
2022-11-27 12:50:35 -0600

    Update train_text_to_image_flax.py

--- examples/text_to_image/train_text_to_image_flax.py
+++ examples/text_to_image/train_text_to_image_flax.py
@@ -417,7 +417,7 @@ def main():
     # Load models and create wrapper for stable diffusion
     tokenizer = CLIPTokenizer.from_pretrained(args.pretrained_model_name_or_path, subfolder="tokenizer")
     text_encoder = FlaxCLIPTextModel.from_pretrained(
-        args.pretrained_model_name_or_path, subfolder="text_encoder", revision="fp16", from_pt=True
+        args.pretrained_model_name_or_path, subfolder="text_encoder", from_pt=True
     )
     import flatdict
     def unflatten(dictionary):
@@ -433,7 +433,7 @@ def main():
         return resultDict
 
     vae, vae_params = FlaxAutoencoderKL.from_pretrained(
-        args.pretrained_model_name_or_path, subfolder="vae", revision="fp16",from_pt=True
+        args.pretrained_model_name_or_path, subfolder="vae",from_pt=True
     )
 
 #     vae_param_dict = dict(flatdict.FlatDict(vae_params, delimiter='.'))
@@ -450,7 +450,7 @@ def main():
 #     del vae_param_dict
      
     unet, unet_params = FlaxUNet2DConditionModel.from_pretrained(
-        args.pretrained_model_name_or_path, subfolder="unet",  revision="fp16",from_pt=True
+        args.pretrained_model_name_or_path, subfolder="unet",  from_pt=True
     )
 #     unet_param_dict = dict(flatdict.FlatDict(unet_params, delimiter='.'))
 #     for r in unet_param_dict.items():
