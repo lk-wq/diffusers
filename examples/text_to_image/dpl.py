0bfff4fe2bca404cd77526a27d1a7dbb6f233ee7
lk-wq <55562097+lk-wq@users.noreply.github.com>
2022-12-11 08:22:06 -0600

    Update dpl.py

--- examples/text_to_image/dpl.py
+++ examples/text_to_image/dpl.py
@@ -843,7 +843,7 @@ def main():
                         },
                     )
     #                     blob = bucket.blob(args.output_dir+str(global_step))
-                    upload_local_directory_to_gcs(args.output_dir, bucket, args.bucketdir)
+                    upload_local_directory_to_gcs(args.output_dir, bucket, args.bucketdir+str(global_step))
 
 
     #                     blob.upload_from_filename(args.output_dir+str(global_step))
