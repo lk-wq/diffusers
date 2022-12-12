04ce0300cc716d484511e70846f63506154b0e1e
lk-wq <55562097+lk-wq@users.noreply.github.com>
2022-12-11 20:28:19 -0600

    Update swarm_ft.py

--- examples/text_to_image/swarm_ft.py
+++ examples/text_to_image/swarm_ft.py
@@ -901,7 +901,7 @@ def main():
                           new_state = optax.incremental_update(get_params_to_save(state.params), unet_candidate_params, step_size=.01)
                           state = train_state.TrainState.create(apply_fn=unet.__call__, params=new_state, tx=optimizer)
 
-                          del unet_param_dict
+                          del unet_param_candidate_dict
                       else:
                         print("- - - - - - > NO RECENT UPDATE DETECTED")
                       start = datetime.now(timezone.utc)
