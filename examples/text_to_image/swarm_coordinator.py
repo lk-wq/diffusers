de893c9330d03f960c48f14daf5fb5873dbf0f14
lk-wq <55562097+lk-wq@users.noreply.github.com>
2022-12-11 18:17:33 -0600

    Update swarm_coordinator.py

--- examples/text_to_image/swarm_coordinator.py
+++ examples/text_to_image/swarm_coordinator.py
@@ -429,6 +429,7 @@ def main():
       for ix , remote_path in enumerate(files_list):
 #        remote_path = os.path.join(gcs_path, local_file[1 + len(local_path):])
         print("remote path is --->",remote_path)
+        print("local path is ---->", local_path)
         blobs = bucket.list_blobs(prefix=remote_path) 
         for blob in blobs:
             filename = blob.name.replace('/', '_')
@@ -439,13 +440,13 @@ def main():
             if 'unet' in filename and last_modified > last_update_time:
                 import os
                 try:
-                    dir_ = local_path+str(ix)+'/unet'
+                    dir_ = local_path+'/'+str(ix)+'/unet'
                     os.mkdir(dir_)
                 except:
                     pass
                 print("file name and dir_",filename,dir_)
                 blob.download_to_filename(dir_+'/'+filename)  # Download
-                update_list.append(local_path+str(ix))                
+                update_list.append(local_pat+'/'+str(ix))                
 #             blob.download_to_filename(local_path+str(ix))
         return update_list
     
