28c0557eaa599e74ec1ce77ed6050c63f0e338f5
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-02-03 11:55:51 -0600

    Update attention.py

--- src/diffusers/models/attention.py
+++ src/diffusers/models/attention.py
@@ -161,13 +161,15 @@ class AttentionBlock(nn.Module):
 #             attention_probs = torch.softmax(attention_scores.float(), dim=-1).type(attention_scores.dtype)
 #             hidden_states = torch.bmm(attention_probs, value_proj).float()
             h = []
-            for _ in range(query_proj.size(1)):
-                attention_scores = scale*(key_proj @ query_proj.transpose(-1,-2)[:,:,_].T).squeeze(-1)
+            scaler = 16384
+            for _ in range(query_proj.size(1)//scaler + 1):
+                attention_scores = scale*(key_proj @ query_proj.transpose(-1,-2)[:,:,scaler*_:scaler*(_+1)].T).squeeze(-1)
                 attention_probs = torch.softmax(attention_scores.float(), dim=-1).type(attention_scores.dtype)
                 # print(attention_probs)
                 hidden_states = attention_probs @ value_proj
                 h.append(hidden_states) 
-            hidden_states = torch.cat(h).squeeze().unsqueeze(0)
+#             hidden_states = torch.cat(h).squeeze().unsqueeze(0)
+            hidden_states = torch.cat(h,dim=1).squeeze().unsqueeze(0)
         # reshape hidden_states
         hidden_states = self.reshape_batch_dim_to_heads(hidden_states)
 
