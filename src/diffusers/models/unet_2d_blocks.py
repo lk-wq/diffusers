9fd99b911aff1a24a9432dba6f52d2bd966d0556
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-02-04 15:04:59 -0600

    Update unet_2d_blocks.py

--- src/diffusers/models/unet_2d_blocks.py
+++ src/diffusers/models/unet_2d_blocks.py
@@ -18,7 +18,7 @@ from torch import nn
 from .attention import AttentionBlock
 from .cross_attention import CrossAttention, CrossAttnAddedKVProcessor
 from .dual_transformer_2d import DualTransformer2DModel
-from .resnet import Downsample2D, FirDownsample2D, FirUpsample2D, ResnetBlock2D, Upsample2D
+from .resnet import Downsample2D, FirDownsample2D, FirUpsample2D, ResnetBlock2D, Upsample2D, ResnetBlock2DCircular
 from .transformer_2d import Transformer2DModel
 
 
@@ -401,6 +401,86 @@ class UNetMidBlock2D(nn.Module):
 
         return hidden_states
 
+class UNetMidBlock2DCircular(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        temb_channels: int,
+        dropout: float = 0.0,
+        num_layers: int = 1,
+        resnet_eps: float = 1e-6,
+        resnet_time_scale_shift: str = "default",
+        resnet_act_fn: str = "swish",
+        resnet_groups: int = 32,
+        resnet_pre_norm: bool = True,
+        add_attention: bool = True,
+        attn_num_head_channels=1,
+        output_scale_factor=1.0,
+    ):
+        super().__init__()
+        resnet_groups = resnet_groups if resnet_groups is not None else min(in_channels // 4, 32)
+        self.add_attention = add_attention
+
+        # there is always at least one resnet
+        resnets = [
+            ResnetBlock2DCircular(
+                in_channels=in_channels,
+                out_channels=in_channels,
+                temb_channels=temb_channels,
+                eps=resnet_eps,
+                groups=resnet_groups,
+                dropout=dropout,
+                time_embedding_norm=resnet_time_scale_shift,
+                non_linearity=resnet_act_fn,
+                output_scale_factor=output_scale_factor,
+                pre_norm=resnet_pre_norm,
+            )
+        ]
+        attentions = []
+
+        for _ in range(num_layers):
+            if self.add_attention:
+                attentions.append(
+                    AttentionBlock(
+                        in_channels,
+                        num_head_channels=attn_num_head_channels,
+                        rescale_output_factor=output_scale_factor,
+                        eps=resnet_eps,
+                        norm_num_groups=resnet_groups,
+                    ).half()
+                )
+            else:
+                attentions.append(None)
+
+            resnets.append(
+                ResnetBlock2DCircular(
+                    in_channels=in_channels,
+                    out_channels=in_channels,
+                    temb_channels=temb_channels,
+                    eps=resnet_eps,
+                    groups=resnet_groups,
+                    dropout=dropout,
+                    time_embedding_norm=resnet_time_scale_shift,
+                    non_linearity=resnet_act_fn,
+                    output_scale_factor=output_scale_factor,
+                    pre_norm=resnet_pre_norm,
+                )
+            )
+
+        self.attentions = nn.ModuleList(attentions)
+        self.resnets = nn.ModuleList(resnets)
+
+    def forward(self, hidden_states, temb=None):
+        hidden_states = self.resnets[0](hidden_states, temb)
+        print("rez",len(self.resnets))
+        for attn, resnet in zip(self.attentions, self.resnets[1:]):
+            if attn is not None:
+                print("attn hit")
+                hidden_states = attn(hidden_states)
+            print("res hit")
+            hidden_states = resnet(hidden_states, temb)
+
+        return hidden_states
 
 class UNetMidBlock2DCrossAttn(nn.Module):
     def __init__(
