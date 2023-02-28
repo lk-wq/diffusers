850f9305e0c9dbfdeb834743ffd823877c872169
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-02-28 09:16:58 -0600

    Update and rename dp_smft4.py to dp_smft5.py

--- examples/text_to_image/dp_smft5.py
+++ examples/text_to_image/dp_smft5.py
@@ -259,7 +259,7 @@ def parse_args():
     parser.add_argument(
         "--resolution",
         type=int,
-        default=512,
+        default=768,
         help=(
             "The resolution for input images, all the images in the train/validation dataset will be resized to this"
             " resolution"
