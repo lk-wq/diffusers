7093090c4d529a0b1cc70fcc6919902cdda06435
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-03-05 12:56:31 -0600

    Update and rename dp_w_te2.py to dp_w_te3.py

--- examples/text_to_image/dp_w_te3.py
+++ examples/text_to_image/dp_w_te3.py
@@ -747,7 +747,10 @@ def main():
             # )[0]
 
             # Predict the noise residual and compute loss
-            unet_outputs = unet.apply({"params": params}, noisy_latents, timesteps, encoder_hidden_states, train=True)
+            unet_outputs = unet.apply(
+                {"params": params["unet"]}, noisy_latents, timesteps, encoder_hidden_states, train=True
+            )
+
             # unet_outputs_unc = unet.apply({"params": params}, noisy_latents, timesteps, encoder_hidden_states_unc, train=True)
 
             noise_pred = unet_outputs.sample
