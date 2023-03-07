91b25ba0dd26b2667f643e210c70554a1769daf4
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-03-07 17:23:13 -0600

    Update and rename dp_smft20.py to dp_smft21.py

--- examples/text_to_image/dp_smft21.py
+++ examples/text_to_image/dp_smft21.py
@@ -509,7 +509,8 @@ def main():
 #         return input_ids
     tokenizer = CLIPTokenizer.from_pretrained(args.pretrained_model_name_or_path, subfolder="tokenizer")
 
-    dataset = FolderData(args.train_data_dir,args.pretrained_model_name_or_path,negative_prompt=args.negative_prompt,section0=args.section0,section1=args.section1,if_=args.img_folder,ip=args.instance_prompt)
+    instance_prompt = args.instance_prompt.replace('_'," ")
+    dataset = FolderData(args.train_data_dir,args.pretrained_model_name_or_path,negative_prompt=args.negative_prompt,section0=args.section0,section1=args.section1,ip=instance_prompt)
 
     def tokenize_captions(captions, is_train=True):
 #         captions = [].
