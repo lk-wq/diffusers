469d31cf6e787c25b47c8dbf4f7c9c7c4ae7796c
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-03-07 17:22:34 -0600

    Update and rename dpl_768_9.py to dpl_768_10.py

--- examples/text_to_image/dpl_768_10.py
+++ examples/text_to_image/dpl_768_10.py
@@ -467,8 +467,8 @@ def main():
 #         input_ids = inputs.input_ids
 #         return input_ids
     tokenizer = CLIPTokenizer.from_pretrained(args.pretrained_model_name_or_path, subfolder="tokenizer")
-
-    dataset = FolderData(args.train_data_dir,args.pretrained_model_name_or_path,negative_prompt=args.negative_prompt,section0=args.section0,section1=args.section1,ip=args.instance_prompt)
+    instance_prompt = args.instance_prompt.replace('_'," ")
+    dataset = FolderData(args.train_data_dir,args.pretrained_model_name_or_path,negative_prompt=args.negative_prompt,section0=args.section0,section1=args.section1,ip=instance_prompt)
 
     def tokenize_captions(captions, is_train=True):
 #         captions = [].
