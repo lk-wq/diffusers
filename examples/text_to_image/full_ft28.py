dd85e478ed9a38fcf9fe273f4e200a97920024e0
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-03-22 12:44:54 -0500

    Update and rename full_ft27.py to full_ft28.py

--- examples/text_to_image/full_ft28.py
+++ examples/text_to_image/full_ft28.py
@@ -686,6 +686,8 @@ def main():
         lambda path, _: 'adam' if any([check_str(i) for i in path]) else 'none')
     def check_str(s):
       if 'attn' in s or 'norm' in s or 'text' in s or 'att' in s or 'bias' in s:
+        if 'text' in s:
+            print(s)
         return True
       return False
     
