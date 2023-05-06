9a222d58bf433318aa1ddf18c5aab6e7284a2464
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-06 15:43:36 -0500

    Update modeling_flax_pytorch_utils.py

--- src/diffusers/models/modeling_flax_pytorch_utils.py
+++ src/diffusers/models/modeling_flax_pytorch_utils.py
@@ -100,14 +100,14 @@ def convert_pytorch_state_dict_to_flax(pt_state_dict, flax_model, init_key=42):
     # Need to change some parameters name to match Flax names
     for pt_key, pt_tensor in pt_state_dict.items():
         renamed_pt_key = rename_key(pt_key)
-        if 'down' in renamed_pt_key:
+        if True:
             print("key", renamed_pt_key)
         pt_tuple_key = tuple(renamed_pt_key.split("."))
 
         # Correctly rename weight parameters
         
         flax_key, flax_tensor = rename_key_and_reshape_tensor(pt_tuple_key, pt_tensor, random_flax_state_dict)
-        if 'down' in flax_key:
+        if True:
             print("flax key",flax_key)
         if flax_key in random_flax_state_dict:
             if flax_tensor.shape != random_flax_state_dict[flax_key].shape:
