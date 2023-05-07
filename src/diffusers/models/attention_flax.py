8844f2ef53e241845a58b26171aaff3c45fd50b0
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-07 09:15:23 -0500

    Update attention_flax.py

--- src/diffusers/models/attention_flax.py
+++ src/diffusers/models/attention_flax.py
@@ -486,7 +486,9 @@ class FlaxBasicTransformerBlock2(nn.Module):
         print("value 5 ",value.shape )
 
         hidden_states = self.attn1(query, key, value)#attention_mask)
-        hidden_states = jnp.transpose(hidden_states,(1, 2)).reshape(batch_size, -1, residual.shape[1])
+        print("hidden after self attn1",hidden_states.shape)
+        print("res shape",residual.shape)
+        hidden_states = jnp.transpose(hidden_states,(0,2, 1)).reshape(batch_size, -1, residual.shape[1])
 
 #         hidden_states = self.batch_to_head_dim(hidden_states)
         
