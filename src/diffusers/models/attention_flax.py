d11ba507808b2f3369ec39607ff33427938be6cb
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-06 19:40:35 -0500

    Update attention_flax.py

--- src/diffusers/models/attention_flax.py
+++ src/diffusers/models/attention_flax.py
@@ -214,8 +214,84 @@ class FlaxAttention(nn.Module):
 
         hidden_states = self.reshape_batch_dim_to_heads(hidden_states)
         hidden_states = self.proj_attn(hidden_states)
-        return hidden_states
+        return attention_probs
 
+class FlaxAttention2(nn.Module):
+    r"""
+    A Flax multi-head attention module as described in: https://arxiv.org/abs/1706.03762
+
+    Parameters:
+        query_dim (:obj:`int`):
+            Input hidden states dimension
+        heads (:obj:`int`, *optional*, defaults to 8):
+            Number of heads
+        dim_head (:obj:`int`, *optional*, defaults to 64):
+            Hidden states dimension inside each head
+        dropout (:obj:`float`, *optional*, defaults to 0.0):
+            Dropout rate
+        use_memory_efficient_attention (`bool`, *optional*, defaults to `False`):
+            enable memory efficient attention https://arxiv.org/abs/2112.05682
+        dtype (:obj:`jnp.dtype`, *optional*, defaults to jnp.float32):
+            Parameters `dtype`
+
+    """
+    query_dim: int
+    heads: int = 8
+    dim_head: int = 64
+    dropout: float = 0.0
+    use_memory_efficient_attention: bool = False
+    dtype: jnp.dtype = jnp.float32
+
+    def setup(self):
+        inner_dim = self.dim_head * self.heads
+        self.scale = self.dim_head**-0.5
+
+        # Weights were exported with old names {to_q, to_k, to_v, to_out}
+#         self.query = nn.Dense(inner_dim, use_bias=False, dtype=self.dtype, name="to_q")
+#         self.key = nn.Dense(inner_dim, use_bias=False, dtype=self.dtype, name="to_k")
+#         self.value = nn.Dense(inner_dim, use_bias=False, dtype=self.dtype, name="to_v")
+
+#         self.proj_attn = nn.Dense(self.query_dim, dtype=self.dtype, name="to_out_0")
+
+    def reshape_heads_to_batch_dim(self, tensor):
+        batch_size, seq_len, dim = tensor.shape
+        head_size = self.heads
+        tensor = tensor.reshape(batch_size, seq_len, head_size, dim // head_size)
+        tensor = jnp.transpose(tensor, (0, 2, 1, 3))
+        tensor = tensor.reshape(batch_size * head_size, seq_len, dim // head_size)
+        return tensor
+
+    def reshape_batch_dim_to_heads(self, tensor):
+        batch_size, seq_len, dim = tensor.shape
+        head_size = self.heads
+        tensor = tensor.reshape(batch_size // head_size, head_size, seq_len, dim)
+        tensor = jnp.transpose(tensor, (0, 2, 1, 3))
+        tensor = tensor.reshape(batch_size // head_size, seq_len, dim * head_size)
+        return tensor
+
+    def __call__(self, query, key, value, deterministic=True):
+#         context = hidden_states if context is None else context
+
+#         query_proj = self.query(hidden_states)
+#         key_proj = self.key(context)
+#         value_proj = self.value(context)
+        
+
+        query_states = self.reshape_heads_to_batch_dim(query_proj)
+        key_states = self.reshape_heads_to_batch_dim(key_proj)
+#         value_states = self.reshape_heads_to_batch_dim(value_proj)
+
+        # compute attentions
+        attention_scores = jnp.einsum("b i d, b j d->b i j", query_states, key_states)
+        attention_scores = attention_scores * self.scale
+        attention_probs = nn.softmax(attention_scores, axis=2)
+
+            # attend to values
+        hidden_states = jnp.einsum("b i j, b j d -> b i d", attention_probs, value_states)
+
+#         hidden_states = self.reshape_batch_dim_to_heads(hidden_states)
+#         hidden_states = self.proj_attn(hidden_states)
+        return attention_probs
 
 class FlaxBasicTransformerBlock(nn.Module):
     r"""
@@ -249,11 +325,7 @@ class FlaxBasicTransformerBlock(nn.Module):
 
     def setup(self):
         # self attention (or cross_attention if only_cross_attention is True)
-        self.attn1 = FlaxAttention(
-            self.dim, self.n_heads, self.d_head, self.dropout, self.use_memory_efficient_attention, dtype=self.dtype
-        )
-        # cross attention
-        self.attn2 = FlaxAttention(
+        self.attn1 = FlaxAttention2(
             self.dim, self.n_heads, self.d_head, self.dropout, self.use_memory_efficient_attention, dtype=self.dtype
         )
         self.ff = FlaxFeedForward(dim=self.dim, dropout=self.dropout, dtype=self.dtype)
@@ -268,12 +340,19 @@ class FlaxBasicTransformerBlock(nn.Module):
             hidden_states = self.attn1(self.norm1(hidden_states), context, deterministic=deterministic)
         else:
             hidden_states = self.attn1(self.norm1(hidden_states), deterministic=deterministic)
+            
+        hidden_states = jnp.einsum("b i j, b j d -> b i d", attention_probs, value_states)
+
+        hidden_states = self.reshape_batch_dim_to_heads(hidden_states)
+        hidden_states = self.proj_attn(hidden_states)
+
+        
         hidden_states = hidden_states + residual
 
         # cross attention
-        residual = hidden_states
-        hidden_states = self.attn2(self.norm2(hidden_states), context, deterministic=deterministic)
-        hidden_states = hidden_states + residual
+#         residual = hidden_states
+#         hidden_states = self.attn2(self.norm2(hidden_states), context, deterministic=deterministic)
+#         hidden_states = hidden_states + residual
 
         # feed forward
         residual = hidden_states
@@ -281,7 +360,202 @@ class FlaxBasicTransformerBlock(nn.Module):
         hidden_states = hidden_states + residual
 
         return hidden_states
+    
+def head_to_batch_dim( tensor):
+    batch_size, seq_len, dim = tensor.shape
+    head_size = self.heads
+    tensor = tensor.reshape(batch_size, seq_len, head_size, dim // head_size)
+    tensor = jnp.transpose(tensor, (0, 2, 1, 3))
+    tensor = tensor.reshape(batch_size * head_size, seq_len, dim // head_size)
+    return tensor
 
+def batch_to_head_dim( tensor):
+    batch_size, seq_len, dim = tensor.shape
+    head_size = self.heads
+    tensor = tensor.reshape(batch_size // head_size, head_size, seq_len, dim)
+    tensor = jnp.transpose(tensor, (0, 2, 1, 3))
+    tensor = tensor.reshape(batch_size // head_size, seq_len, dim * head_size)
+    return tensor
+
+class FlaxBasicTransformerBlock2(nn.Module):
+    r"""
+    A Flax transformer block layer with `GLU` (Gated Linear Unit) activation function as described in:
+    https://arxiv.org/abs/1706.03762
+
+
+    Parameters:
+        dim (:obj:`int`):
+            Inner hidden states dimension
+        n_heads (:obj:`int`):
+            Number of heads
+        d_head (:obj:`int`):
+            Hidden states dimension inside each head
+        dropout (:obj:`float`, *optional*, defaults to 0.0):
+            Dropout rate
+        only_cross_attention (`bool`, defaults to `False`):
+            Whether to only apply cross attention.
+        dtype (:obj:`jnp.dtype`, *optional*, defaults to jnp.float32):
+            Parameters `dtype`
+        use_memory_efficient_attention (`bool`, *optional*, defaults to `False`):
+            enable memory efficient attention https://arxiv.org/abs/2112.05682
+    """
+    dim: int
+    n_heads: int
+    d_head: int
+    dropout: float = 0.0
+    only_cross_attention: bool = False
+    dtype: jnp.dtype = jnp.float32
+    use_memory_efficient_attention: bool = False
+
+    def setup(self):
+        # self attention (or cross_attention if only_cross_attention is True)
+        self.attn1 = FlaxAttention2(
+            self.dim, self.n_heads, self.d_head, self.dropout, self.use_memory_efficient_attention, dtype=self.dtype
+        )
+#         self.ff = FlaxFeedForward(dim=self.dim, dropout=self.dropout, dtype=self.dtype)
+#         self.norm1 = nn.LayerNorm(epsilon=1e-5, dtype=self.dtype)
+#         self.norm2 = nn.LayerNorm(epsilon=1e-5, dtype=self.dtype)
+#         self.norm3 = nn.LayerNorm(epsilon=1e-5, dtype=self.dtype)
+    def __call__(self, hidden_states, encoder_hidden_states_key_proj,attn, deterministic=True):
+        residual = hidden_states
+        hidden_states = hidden_states.view(hidden_states.shape[0], hidden_states.shape[1], -1).transpose(1, 2)
+        batch_size, sequence_length, _ = hidden_states.shape
+        if encoder_hidden_states_key_proj is None:
+            encoder_hidden_states_key_proj = hidden_states
+        else:
+            encoder_hidden_states_key_proj = attn.norm_cross(encoder_hidden_states_key_proj)
+        hidden_states = attn.group_norm(hidden_states)#.transpose(1, 2)).transpose(1, 2)
+
+        query = attn.to_q(hidden_states)
+        query = head_to_batch_dim(query, out_dim=4)
+
+        encoder_hidden_states_key_proj = attn.add_k_proj(encoder_hidden_states)
+        encoder_hidden_states_value_proj = attn.add_v_proj(encoder_hidden_states)
+        encoder_hidden_states_key_proj = attn.head_to_batch_dim(encoder_hidden_states_key_proj, out_dim=4)
+        encoder_hidden_states_value_proj = attn.head_to_batch_dim(encoder_hidden_states_value_proj, out_dim=4)
+
+        key = attn.to_k(hidden_states)
+        value = attn.to_v(hidden_states)
+        key = attn.head_to_batch_dim(key, out_dim=4)
+        value = attn.head_to_batch_dim(value, out_dim=4)
+        key = torch.cat([encoder_hidden_states_key_proj, key], dim=2)
+        value = torch.cat([encoder_hidden_states_value_proj, value], dim=2)
+        
+        hidden_states = self.attn1(query, key, attention_mask)
+
+        hidden_states = batch_to_head_dim(hidden_states)
+        
+        hidden_states = attn.to_out[0](hidden_states)
+
+        hidden_states = hidden_states + residual
+
+        return hidden_states
+
+class FlaxTransformer2DModel2(nn.Module):
+    r"""
+    A Spatial Transformer layer with Gated Linear Unit (GLU) activation function as described in:
+    https://arxiv.org/pdf/1506.02025.pdf
+
+
+    Parameters:
+        in_channels (:obj:`int`):
+            Input number of channels
+        n_heads (:obj:`int`):
+            Number of heads
+        d_head (:obj:`int`):
+            Hidden states dimension inside each head
+        depth (:obj:`int`, *optional*, defaults to 1):
+            Number of transformers block
+        dropout (:obj:`float`, *optional*, defaults to 0.0):
+            Dropout rate
+        use_linear_projection (`bool`, defaults to `False`): tbd
+        only_cross_attention (`bool`, defaults to `False`): tbd
+        dtype (:obj:`jnp.dtype`, *optional*, defaults to jnp.float32):
+            Parameters `dtype`
+        use_memory_efficient_attention (`bool`, *optional*, defaults to `False`):
+            enable memory efficient attention https://arxiv.org/abs/2112.05682
+    """
+    in_channels: int
+    n_heads: int
+    d_head: int
+    depth: int = 1
+    dropout: float = 0.0
+    use_linear_projection: bool = False
+    only_cross_attention: bool = False
+    dtype: jnp.dtype = jnp.float32
+    use_memory_efficient_attention: bool = False
+
+    def setup(self):
+        self.norm_cross = nn.GroupNorm(num_groups=32, epsilon=1e-5)
+
+        inner_dim = self.n_heads * self.d_head
+#         if self.use_linear_projection:
+#             self.proj_in = nn.Dense(inner_dim, dtype=self.dtype)
+#         else:
+#             self.proj_in = nn.Conv(
+#                 inner_dim,
+#                 kernel_size=(1, 1),
+#                 strides=(1, 1),
+#                 padding="VALID",
+#                 dtype=self.dtype,
+#               self.to_out = []
+        self.to_out.append(nn.Dense(query_dim))
+#         self.to_out.append(nn.Dropout(dropout))
+        self.to_k = nn.Dense(768)
+        self.to_q = nn.Dense(768)
+        self.to_v = nn.Dense(768)
+        self.group_norm = nn.GroupNorm(num_groups=32, epsilon=1e-5)
+        self.norm_cross = nn.GroupNorm(num_groups=32, epsilon=1e-5)
+        self.add_k_proj = nn.Dense(768)
+        self.add_v_proj = nn.Dense(768)
+
+        self.transformer_blocks = [
+            FlaxBasicTransformerBlock2(
+                inner_dim,
+                self.n_heads,
+                self.d_head,
+                dropout=self.dropout,
+                only_cross_attention=self.only_cross_attention,
+                dtype=self.dtype,
+                use_memory_efficient_attention=self.use_memory_efficient_attention,
+            )
+            for _ in range(1)
+        ]
+
+#         if self.use_linear_projection:
+#             self.to_out = nn.Dense(inner_dim, dtype=self.dtype)
+#         else:
+#             self.to_out = nn.Conv(
+#                 inner_dim,
+#                 kernel_size=(1, 1),
+#                 strides=(1, 1),
+#                 padding="VALID",
+#                 dtype=self.dtype,
+#             )
+
+    def __call__(self, hidden_states, context, deterministic=True):
+#         batch, height, width, channels = hidden_states.shape
+#         residual = hidden_states
+#         hidden_states = self.norm_cross(hidden_states)
+#         if self.use_linear_projection:
+#             hidden_states = hidden_states.reshape(batch, height * width, channels)
+#             hidden_states = self.proj_in(hidden_states)
+#         else:
+#             hidden_states = self.proj_in(hidden_states)
+#             hidden_states = hidden_states.reshape(batch, height * width, channels)
+
+        for transformer_block in self.transformer_blocks:
+            hidden_states = transformer_block(hidden_states, context, deterministic=deterministic)
+
+#         if self.use_linear_projection:
+#             hidden_states = self.to_out(hidden_states)
+#             hidden_states = hidden_states.reshape(batch, height, width, channels)
+#         else:
+#             hidden_states = hidden_states.reshape(batch, height, width, channels)
+#             hidden_states = self.to_out(hidden_states)
+
+#         hidden_states = hidden_states + residual
+        return hidden_states
 
 class FlaxTransformer2DModel(nn.Module):
     r"""
@@ -333,7 +607,7 @@ class FlaxTransformer2DModel(nn.Module):
             )
 
         self.transformer_blocks = [
-            FlaxBasicTransformerBlock(
+            FlaxBasicTransformerBlock2(
                 inner_dim,
                 self.n_heads,
                 self.d_head,
@@ -346,9 +620,9 @@ class FlaxTransformer2DModel(nn.Module):
         ]
 
         if self.use_linear_projection:
-            self.proj_out = nn.Dense(inner_dim, dtype=self.dtype)
+            self.to_out = nn.Dense(inner_dim, dtype=self.dtype)
         else:
-            self.proj_out = nn.Conv(
+            self.to_out = nn.Conv(
                 inner_dim,
                 kernel_size=(1, 1),
                 strides=(1, 1),
@@ -357,25 +631,25 @@ class FlaxTransformer2DModel(nn.Module):
             )
 
     def __call__(self, hidden_states, context, deterministic=True):
-        batch, height, width, channels = hidden_states.shape
-        residual = hidden_states
-        hidden_states = self.norm_cross(hidden_states)
-        if self.use_linear_projection:
-            hidden_states = hidden_states.reshape(batch, height * width, channels)
-            hidden_states = self.proj_in(hidden_states)
-        else:
-            hidden_states = self.proj_in(hidden_states)
-            hidden_states = hidden_states.reshape(batch, height * width, channels)
+#         batch, height, width, channels = hidden_states.shape
+#         residual = hidden_states
+#         hidden_states = self.norm_cross(hidden_states)
+#         if self.use_linear_projection:
+#             hidden_states = hidden_states.reshape(batch, height * width, channels)
+#             hidden_states = self.proj_in(hidden_states)
+#         else:
+#             hidden_states = self.proj_in(hidden_states)
+#             hidden_states = hidden_states.reshape(batch, height * width, channels)
 
         for transformer_block in self.transformer_blocks:
             hidden_states = transformer_block(hidden_states, context, deterministic=deterministic)
 
         if self.use_linear_projection:
-            hidden_states = self.proj_out(hidden_states)
+            hidden_states = self.to_out(hidden_states)
             hidden_states = hidden_states.reshape(batch, height, width, channels)
         else:
             hidden_states = hidden_states.reshape(batch, height, width, channels)
-            hidden_states = self.proj_out(hidden_states)
+            hidden_states = self.to_out(hidden_states)
 
         hidden_states = hidden_states + residual
         return hidden_states
