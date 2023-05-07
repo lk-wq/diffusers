99e613fa6a56fd63ea5f8cdda3f0a3f024187ddd
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-06 19:44:15 -0500

    Update attention_flax.py

--- src/diffusers/models/attention_flax.py
+++ src/diffusers/models/attention_flax.py
@@ -498,7 +498,7 @@ class FlaxTransformer2DModel2(nn.Module):
 #                 strides=(1, 1),
 #                 padding="VALID",
 #                 dtype=self.dtype,
-#               self.to_out = []
+        self.to_out = []
         self.to_out.append(nn.Dense(query_dim))
 #         self.to_out.append(nn.Dropout(dropout))
         self.to_k = nn.Dense(768)
