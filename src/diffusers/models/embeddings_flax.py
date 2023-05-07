2e08148555f82f5576cb441c526eb29082f171e3
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-07 18:51:35 -0500

    Update embeddings_flax.py

--- src/diffusers/models/embeddings_flax.py
+++ src/diffusers/models/embeddings_flax.py
@@ -170,9 +170,9 @@ class FlaxAttentionPooling(nn.Module):
             x = jnp.transpose(x,(0,2, 1))
             return x
         print("x mean vs self pos",jnp.mean(x,axis=1, keepdims=True).shape ,self.positional_embedding.shape)
-        try:
+        if raw_width > 768:
             class_token = jnp.mean(x,axis=1, keepdims=True) + self.positional_embedding
-        except:
+        else:
             class_token = jnp.mean(x,axis=1, keepdims=True)
         x = jnp.concatenate([class_token, x], axis=1)  # (bs, length+1, width)
 
