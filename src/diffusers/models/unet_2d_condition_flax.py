74b4f81ec47f418ef5466a1df5ef30a2abcdaf27
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-07 19:20:49 -0500

    Update unet_2d_condition_flax.py

--- src/diffusers/models/unet_2d_condition_flax.py
+++ src/diffusers/models/unet_2d_condition_flax.py
@@ -298,6 +298,7 @@ class FlaxUNet2DConditionModel(nn.Module, FlaxModelMixin, ConfigMixin):
             t_emb2 = t_emb + self.add_embedding(encoder_hidden_states)
         else:
             print("pre t_emb",t_emb.shape)
+            print("pre emb val",t_emb)
             t_emb = t_emb + self.add_embedding(encoder_hidden_states)
             print("post t_emb",t_emb.shape)
             
