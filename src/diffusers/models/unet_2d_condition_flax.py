926ecfbf61fe6e43baeaa064e5d4526412c0ed20
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-08 12:41:19 -0500

    Update unet_2d_condition_flax.py

--- src/diffusers/models/unet_2d_condition_flax.py
+++ src/diffusers/models/unet_2d_condition_flax.py
@@ -311,7 +311,7 @@ class FlaxUNet2DConditionModel(nn.Module, FlaxModelMixin, ConfigMixin):
 #         print('sample.npy',sample)
 #         print('t_emb.npy 1',t_emb)
 #         print('encoder_hidden_states.npy',encoder_hidden_states)
-        for down_block in self.down_blocks:
+        for ix , down_block in enumerate(self.down_blocks):
             print("db",down_block)
             if isinstance(down_block, FlaxCrossAttnDownBlock2D):
                 print("pre down_block",sample.shape )
@@ -325,6 +325,8 @@ class FlaxUNet2DConditionModel(nn.Module, FlaxModelMixin, ConfigMixin):
                      print("not cross pre down_block",sample.shape)
                 sample, res_samples = down_block(sample, t_emb, deterministic=not train)
                 print("not cross post down_block",sample.shape)
+            if ix == 0:
+                print("sample 1 --------------->",sample)
             down_block_res_samples += res_samples
 
         if down_block_additional_residuals is not None:
