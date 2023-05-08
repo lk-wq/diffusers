c685b55a6f5aa42246b6112ffd7f90bc240fe9fa
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-08 15:33:42 -0500

    Update unet_2d_blocks_flax.py

--- src/diffusers/models/unet_2d_blocks_flax.py
+++ src/diffusers/models/unet_2d_blocks_flax.py
@@ -98,10 +98,10 @@ class FlaxCrossAttnDownBlock2D(nn.Module):
         for ix, (resnet, attn) in enumerate(zip(self.resnets, self.attentions)):
             hidden_states = resnet(hidden_states, temb, deterministic=deterministic)
 #             if ix == 1:
-            print("pre attn",ix,hidden_states,hidden_states.shape)
+            print("pre attn",ix,jnp.transpose( hidden_states, (0,2,3,1)  ),hidden_states.shape)
             hidden_states = attn(hidden_states, encoder_hidden_states, deterministic=deterministic)
 #             if ix == 1:
-            print("post attn",ix,hidden_states,hidden_states.shape)
+            print("post attn",ix,jnp.transpose( hidden_states, (0,2,3,1)  ),hidden_states.shape)
 
             output_states += (hidden_states,)
 
