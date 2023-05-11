e6f7f09654c6ecd5cb0b56599f4c44de32931eb3
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-11 07:20:31 -0500

    Update and rename flax31.py to flax32.py

--- examples/text_to_image/flax32.py
+++ examples/text_to_image/flax32.py
@@ -1030,6 +1030,7 @@ def main():
     noise_scheduler = FlaxDDPMScheduler(
         beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000
     )
+    noise_scheduler_state = noise_scheduler.create_state()
 
     # Initialize our training
     rng = jax.random.PRNGKey(args.seed)
