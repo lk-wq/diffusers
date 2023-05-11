630153ff210dcc00f448e7e31293cec734e71de0
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-11 18:45:47 -0500

    Update and rename flax_text11.py to flax_text12.py

--- examples/text_to_image/flax_text12.py
+++ examples/text_to_image/flax_text12.py
@@ -983,11 +983,11 @@ def main():
         state = optimizer2.init(params)
         return state, params
 
-#     param_spec = set_partitions(unfreeze(params))
+    param_spec = set_partitions(unfreeze(params))
     text_param_spec = set_partitions_text(unfreeze(text_params))
 
-#     params_shapes = jax.tree_util.tree_map(lambda x: x.shape, params)
-#     state_shapes = jax.eval_shape(get_initial_state, params_shapes)
+    params_shapes = jax.tree_util.tree_map(lambda x: x.shape, params)
+    state_shapes = jax.eval_shape(get_initial_state, params_shapes)
 
     text_params_shapes = jax.tree_util.tree_map(lambda x: x.shape, text_params)
     text_state_shapes = jax.eval_shape(get_initial_state2, text_params_shapes)
@@ -1001,26 +1001,26 @@ def main():
             return text_param_spec
         return None
     
-#     opt_state_spec, param_spec = jax.tree_util.tree_map(
-#         get_opt_spec, state_shapes, is_leaf=lambda x: isinstance(x, (dict, optax.EmptyState))
-#     )
+    opt_state_spec, param_spec = jax.tree_util.tree_map(
+        get_opt_spec, state_shapes, is_leaf=lambda x: isinstance(x, (dict, optax.EmptyState))
+    )
 
     text_opt_state_spec, text_param_spec = jax.tree_util.tree_map(
         get_opt_spec2, text_state_shapes, is_leaf=lambda x: isinstance(x, (dict, optax.EmptyState))
     )
 
-#     p_get_initial_state = pjit(
-#         get_initial_state,
-#         in_axis_resources=None,
-#         out_axis_resources=(opt_state_spec, param_spec),
-#     )
-    p_get_initial_state2 = pjit(
-        get_initial_state2,
+    p_get_initial_state = pjit(
+        get_initial_state,
         in_axis_resources=None,
         out_axis_resources=(opt_state_spec, param_spec),
     )
+    p_get_initial_state2 = pjit(
+        get_initial_state2,
+        in_axis_resources=None,
+        out_axis_resources=(text_opt_state_spec, text_param_spec),
+    )
 
-#     params = jax.tree_util.tree_map(lambda x: np.asarray(x), params)
+    params = jax.tree_util.tree_map(lambda x: np.asarray(x), params)
     
 #     mesh_devices = np.array(jax.devices()).reshape(1, jax.local_device_count())
     mesh_devices = mesh_utils.create_device_mesh((4, 2))
@@ -1137,8 +1137,8 @@ def main():
 #     p_train_step = jax.pmap(train_step, "batch", donate_argnums=(0, 1))
     p_train_step = pjit(
         train_step,
-        in_axis_resources=(param_spec, opt_state_spec,param_spec, opt_state_spec, None, None),
-        out_axis_resources=(param_spec, opt_state_spec,param_spec, opt_state_spec, None, None),
+        in_axis_resources=(param_spec, opt_state_spec,text_opt_state_spec, text_param_spec, None, None),
+        out_axis_resources=(param_spec, opt_state_spec,text_opt_state_spec, text_param_spec, None, None),
         donate_argnums=(0, 1,2,3),
     )
 #     p_get_initial_state = pjit(
