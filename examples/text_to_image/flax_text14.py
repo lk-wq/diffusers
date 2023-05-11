0d88e2260cb6080270ffee8eb5f775e12ff79c7b
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-11 18:50:36 -0500

    Update and rename flax_text13.py to flax_text14.py

--- examples/text_to_image/flax_text14.py
+++ examples/text_to_image/flax_text14.py
@@ -1021,6 +1021,7 @@ def main():
     )
 
     params = jax.tree_util.tree_map(lambda x: np.asarray(x), params)
+    text_params = jax.tree_util.tree_map(lambda x: np.asarray(x), text_params)
     
 #     mesh_devices = np.array(jax.devices()).reshape(1, jax.local_device_count())
     mesh_devices = mesh_utils.create_device_mesh((4, 2))
