1cbfc28152631b4af2995f6ebc0f6005ce95d4a0
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-12 17:34:28 -0500

    Update unet_2d_condition.py

--- src/diffusers/models/unet_2d_condition.py
+++ src/diffusers/models/unet_2d_condition.py
@@ -749,7 +749,7 @@ class UNet2DConditionModel(ModelMixin, ConfigMixin, UNet2DConditionLoadersMixin)
                         encoder_hidden_states=encoder_hidden_states,
                         attention_mask=attention_mask,
                         cross_attention_kwargs=cross_attention_kwargs,
-                        save=False
+                        save=True
                     )
                 else:
                     sample, res_samples = downsample_block(
@@ -766,7 +766,7 @@ class UNet2DConditionModel(ModelMixin, ConfigMixin, UNet2DConditionLoadersMixin)
                 print("entering init sample",ix , sample)
                 print(" ")
                 print("entering init emb",ix , emb)
-                sample, res_samples = downsample_block(hidden_states=sample, temb=emb,save=True)
+                sample, res_samples = downsample_block(hidden_states=sample, temb=emb,save=False)
             if ix == 0:
                 torch.save(sample,'sample_d1.pth')
             down_block_res_samples += res_samples
