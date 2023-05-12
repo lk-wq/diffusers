b6d029ade5c530372094844f8a4b863fb63aa84d
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-12 12:08:21 -0500

    Update and rename flax_text45.py to flax_text46.py

--- examples/text_to_image/flax_text46.py
+++ examples/text_to_image/flax_text46.py
@@ -1262,8 +1262,8 @@ def main():
     epochs = tqdm(range(args.num_train_epochs), desc="Epoch ... ", position=0)
 #     avg = get_params_to_save(state.params)
 #     text_avg = get_params_to_save(text_encoder_state.params)
-    avg = ema_update( rng, jax.device_get(unet_params) , avg, decay )
-    text_avg = ema_update(rng, jax.device_get(text_params) , text_avg, decay )
+    avg = jax.device_get(unet_params) #ema_update( rng, jax.device_get(unet_params) , avg, decay )
+    text_avg = jax.device_get(text_params) #ema_update(rng, jax.device_get(text_params) , text_avg, decay )
 
     client = storage.Client()
     bucket = client.bucket(args.bucketname)
