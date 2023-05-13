6b67406a87983a0957ff32cfc85ed829a8a45285
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-13 17:44:18 -0500

    Update partitions_simple.py

--- examples/text_to_image/partitions_simple.py
+++ examples/text_to_image/partitions_simple.py
@@ -113,6 +113,6 @@ def set_partitions(in_dict):
     rules = _get_partition_rules()
     replace = _replacement_rules(rules)
 #     initd = {k: _unmatched for k in flatten_dict(in_dict)}
-    result = {k: replace(k, v.shape) for k, v in result.items()}
+    result = {k: replace(k, v.shape) for k, v in flatten_dict(in_dict).items()}
     assert _unmatched not in result.values(), "Incomplete partition spec."
     return freeze(unflatten_dict(result))
