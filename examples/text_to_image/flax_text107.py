38facf55610cb8fce2f80f2000e95f3e2a9eb0cb
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-13 17:26:16 -0500

    Update and rename flax_text106.py to flax_text107.py

--- examples/text_to_image/flax_text107.py
+++ examples/text_to_image/flax_text107.py
@@ -1217,8 +1217,8 @@ def main():
     from jax.sharding import NamedSharding
 
     mesh = Mesh(mesh_devices , axis_names={'mp','dp'})
-    text_params = jax.tree_util.tree_map(lambda x: jax.device_put(x ,NamedSharding(mesh , partition_shape(x.shape)) ), text_params)
-    unet_params = jax.tree_util.tree_map(lambda x: jax.device_put(x ,NamedSharding(mesh , partition_shape(x.shape)) ), params)
+    text_params = jax.tree_util.tree_map(lambda x: jax.device_put(x ,NamedSharding(mesh , partition_shape(x.shape)) ), freeze(text_params))
+    unet_params = jax.tree_util.tree_map(lambda x: jax.device_put(x ,NamedSharding(mesh , partition_shape(x.shape)) ), freeze(params))
 #     text_params = jax.tree_util.tree_map(lambda x: np.asarray(x), text_params)
 #     text_opt_state = jax.tree_util.tree_map(lambda x: np.asarray(x), text_opt_state)
 
