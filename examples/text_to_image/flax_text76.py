c92563b9643e19c98e52e705593bd67db27a8557
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-13 12:19:17 -0500

    Update and rename flax_text75.py to flax_text76.py

--- examples/text_to_image/flax_text76.py
+++ examples/text_to_image/flax_text76.py
@@ -1064,8 +1064,8 @@ def main():
     def get_initial_state2(params):
 #         params = jax.tree_util.tree_map(lambda x: x, params)
 
-        state = optimizer2.init(params)
-        return state, params
+#         state = optimizer2.init(params)
+        return params
 
     param_spec = set_partitions(unfreeze(params))
     text_param_spec = set_partitions_text(unfreeze(text_params))
@@ -1089,9 +1089,9 @@ def main():
         get_opt_spec, state_shapes, is_leaf=lambda x: isinstance(x, (dict, optax.EmptyState))
     )
 
-    text_opt_state_spec, text_param_spec = jax.tree_util.tree_map(
-        get_opt_spec2, text_state_shapes, is_leaf=lambda x: isinstance(x, (dict, optax.EmptyState))
-    )
+#     text_opt_state_spec, text_param_spec = jax.tree_util.tree_map(
+#         get_opt_spec2, text_state_shapes, is_leaf=lambda x: isinstance(x, (dict, optax.EmptyState))
+#     )
 #     p_get_initial_state2 = pjit(
 #         get_initial_state2,
 #         in_axis_resources=(None,None),
@@ -1130,7 +1130,7 @@ def main():
 
     with Mesh( mesh_devices , ("dp","mp") ):
         f = freeze(text_params)
-        text_opt_state , text_params = p_get_initial_state2( f )
+        text_params = p_get_initial_state2( f )
     return
     del f
 #     del f2
