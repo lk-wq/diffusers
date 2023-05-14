a37092226141b726b296d4a8eb613fa224895801
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-13 21:11:01 -0500

    Update and rename flax_no_unet20.py to flax_no_unet22.py

--- examples/text_to_image/flax_no_unet22.py
+++ examples/text_to_image/flax_no_unet22.py
@@ -1032,8 +1032,6 @@ def main():
         return jax.random.PRNGKey(seed)
     rng = create_key(args.seed)
 
-    optimizer = optax.multi_transform(
-      {'adam': optimizer2_, 'none': optax.set_to_zero()}, label_fn)
     weight_dtype = jnp.bfloat16
     unet, params = FlaxUNet2DConditionModel.from_pretrained(
         args.pretrained_model_name_or_path, subfolder="unet",dtype=weight_dtype
@@ -1042,6 +1040,17 @@ def main():
     text_encoder, text_params = FlaxT5EncoderModel.from_pretrained(
         args.pretrained_model_name_or_path, subfolder="text_encoder",dtype=weight_dtype
     )
+    flat = flax.traverse_util.flatten_dict( text_params )
+    d = {}
+    for i in flat.keys():
+        key = ".".join(list(i))
+        if '23' in key:
+            d[key] = 'adam'
+        else:
+            d[key] = 'none'
+    fd = flax.core.frozen_dict.freeze(d)
+    optimizer = optax.multi_transform(
+      {'adam': optimizer2_, 'none': optax.set_to_zero()}, fd)
 
 #     def get_initial_state(params):
 # #         params = jax.tree_util.tree_map(lambda x: x, params)
@@ -1222,7 +1231,7 @@ def main():
     mesh = Mesh(mesh_devices , axis_names={'dp','mp'})
     text_params = jax.tree_util.tree_map(lambda x: jax.device_put(x ,NamedSharding(mesh , partition_shape(x.shape)) ), freeze(text_params))
     unet_params = jax.tree_util.tree_map(lambda x: jax.device_put(x ,NamedSharding(mesh , partition_shape(x.shape)) ), freeze(params))
-    text_opt_state = freeze(optimizer.init(unfreeze(text_params)))
+    text_opt_state = optimizer.init(text_params)
 #     text_params = jax.tree_util.tree_map(lambda x: np.asarray(x), text_params)
 #     text_opt_state = jax.tree_util.tree_map(lambda x: np.asarray(x), text_opt_state)
 
