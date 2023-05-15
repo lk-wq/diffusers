f44cf0deee545ae7bae318ab68a2074621ca4a3e
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-15 12:27:06 -0500

    Update and rename flax_train12.py to flax_train13.py

--- examples/text_to_image/flax_train13.py
+++ examples/text_to_image/flax_train13.py
@@ -1114,9 +1114,9 @@ def main():
 
             return loss
 
-        grad_fn = jax.value_and_grad(compute_loss)
-#         loss = compute_loss(params)
-        loss, grads = grad_fn(params)
+#         grad_fn = jax.value_and_grad(compute_loss)
+        loss = compute_loss(params)
+#         loss, grads = grad_fn(params)
 #         unet_updates, new_unet_opt_state = optimizer.update(grads['unet'], unet_opt_state, params['unet'])
 #         new_unet_params = optax.apply_updates(params['unet'], unet_updates)
         
