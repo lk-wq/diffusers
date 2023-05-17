a82fe2675e1b68f542349f1d2b7a9d89009cf239
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-16 22:05:26 -0500

    Update and rename f45_5.py to f45_6.py

--- examples/text_to_image/f45_6.py
+++ examples/text_to_image/f45_6.py
@@ -1135,7 +1135,7 @@ def main():
         
 #         text_updates, new_text_opt_state = optimizer.update(grads['text_encoder'], text_opt_state,params['text_encoder'])
 #         new_text_params = optax.apply_updates(params['text_encoder'], text_updates)
-        save_2_(grads['unet'])
+#         save_2_(grads['unet'])
 #         print("grads ------------------------------>",grads['unet'])
         
         metrics = {"loss": loss}
