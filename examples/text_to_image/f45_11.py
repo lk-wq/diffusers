b985f71da00a8911f1bcc83277799b77a145c298
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-17 13:02:07 -0500

    Update and rename f45_10.py to f45_11.py

--- examples/text_to_image/f45_11.py
+++ examples/text_to_image/f45_11.py
@@ -28,7 +28,7 @@ from diffusers import (
 )
 
 from jax import config
-# config.update("jax_debug_nans",True)
+config.update("jax_debug_nans",True)
 # config.update("jax_disable_jit",True)
 # config.update("jax_enable_x64",True)
 
