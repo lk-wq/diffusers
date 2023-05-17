8e9f01451d0b7c849396439ed3678c301f702f1f
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-05-17 14:14:43 -0500

    Update and rename f45_20.py to f45_21.py

--- examples/text_to_image/f45_21.py
+++ examples/text_to_image/f45_21.py
@@ -219,7 +219,7 @@ from jax.experimental import io_callback
 from jax import debug
 import numpy as np
 
-def save_3_(x,name):
+def save_3_(x):
     # print(name , "---------------------------------------------------->",x)
 #     debug.callback(lambda x: np.save(name,x),x ) #np.save('post_conv1.npy',np.asarray(hidden_states))
     debug.callback(lambda x: print( jax.tree_util.tree_map(lambda y:  np.asarray(y) if np.isnan(y).any() else None , grads['unet'])  ),x ) #np.save('post_conv1.npy',np.asarray(hidden_states))
