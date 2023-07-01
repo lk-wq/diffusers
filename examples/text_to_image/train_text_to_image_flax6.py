40caaa4029426c1af833c58abb19a7a03c2b2da4
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-07-01 16:21:57 -0500

    Update train_text_to_image_flax7.py

--- examples/text_to_image/train_text_to_image_flax6.py
+++ examples/text_to_image/train_text_to_image_flax6.py
@@ -332,9 +332,13 @@ def main():
                 # take a random caption if there are multiple
                 captions.append(random.choice(caption) if is_train else caption[0])
             else:
+                # raise ValueError(
+                #     f"Caption column `{caption_column}` should contain either strings or lists of strings."
+                # )
                 raise ValueError(
-                    f"Caption column `{caption_column}` should contain either strings or lists of strings."
+                "fail "
                 )
+
         inputs = tokenizer(captions, max_length=tokenizer.model_max_length, padding="do_not_pad", truncation=True)
         input_ids = inputs.input_ids
         return input_ids
@@ -501,8 +505,11 @@ def main():
                 elif noise_scheduler.config.prediction_type == "v_prediction":
                     target = noise_scheduler.get_velocity(noise_scheduler_state, latents, noise, timesteps)
                 else:
-                    raise ValueError(f"Unknown prediction type {noise_scheduler.config.prediction_type}")
-    
+                    # raise ValueError(f"Unknown prediction type {noise_scheduler.config.prediction_type}")
+                    raise ValueError(
+                    "fail "
+                    )
+
                 loss = (target - model_pred) ** 2
                 loss = loss.mean()
     
@@ -573,7 +580,9 @@ def main():
                 elif noise_scheduler.config.prediction_type == "v_prediction":
                     target = noise_scheduler.get_velocity(noise_scheduler_state, latents, noise, timesteps)
                 else:
-                    raise ValueError(f"Unknown prediction type {noise_scheduler.config.prediction_type}")
+                    raise ValueError(
+                        "fail "
+                    )
     
                 loss = (target - model_pred) ** 2
                 loss = loss.mean()
