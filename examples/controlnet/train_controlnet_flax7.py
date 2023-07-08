f18abb7ead7ba2f303f3f35f41b7113e18dc26d9
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-07-08 13:23:59 -0500

    Update and rename train_controlnet_flax6.py to train_controlnet_flax7.py

--- examples/controlnet/train_controlnet_flax7.py
+++ examples/controlnet/train_controlnet_flax7.py
@@ -951,7 +951,7 @@ def main():
 
         p_train_step = pjit(
             train_step,
-            in_axis_resources=( unet_param_spec,text_param_spec,control_param_spec,opt_state_spec,None,None ),
+            in_axis_resources=( unet_param_spec,text_param_spec,control_param_spec,vae_param_spec,opt_state_spec,None,None ),
             out_axis_resources=(control_param_spec,opt_state_spec,None, None),
             donate_argnums=(2,3),
         )
