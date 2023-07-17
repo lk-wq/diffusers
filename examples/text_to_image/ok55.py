77257e62784fa011c49003c2906478efee1ecac7
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-07-17 09:22:45 -0500

    Update and rename ok54.py to ok55.py

--- examples/text_to_image/ok55.py
+++ examples/text_to_image/ok55.py
@@ -1099,11 +1099,12 @@ def main():
         vae_param_spec = jax.tree_util.tree_map(lambda x: partition_shape(x.shape) , vae_params )
     
         text_params = jax.tree_util.tree_map(lambda x: jax.device_put(x ,NamedSharding(mesh , partition_shape(x.shape)) ).astype(weight_dtype), text_params)
-        unet_params = jax.tree_util.tree_map(lambda x: jax.device_put(x ,NamedSharding(mesh , partition_shape(x.shape)) ).astype(weight_dtype), unet_params)
         vae_params = jax.tree_util.tree_map(lambda x: jax.device_put(x ,NamedSharding(mesh , partition_shape(x.shape)) ).astype(weight_dtype), vae_params)
+        return
+        unet_params = jax.tree_util.tree_map(lambda x: jax.device_put(x ,NamedSharding(mesh , partition_shape(x.shape)) ).astype(weight_dtype), unet_params)
         # del text_encoder
         opt_state = optimizer.init(unet_params)
-        return
+        # return
         unet_opt_state_spec = jax.tree_util.tree_map(lambda x : partition_shape(x.shape), opt_state )
     # flat2 = flax.traverse_util.flatten_dict( unet_params )
     # print('1',flat2[('conv_in','kernel')].shape)
