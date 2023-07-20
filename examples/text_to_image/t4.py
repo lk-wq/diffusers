3295fb1bd5bf69b2c7982c5cf8f7ebc9560000b2
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-07-20 11:19:31 -0500

    Update and rename t3.py to t4.py

--- examples/text_to_image/t4.py
+++ examples/text_to_image/t4.py
@@ -456,7 +456,7 @@ def main():
         lambda path, _: 'adam' if check_str2(path) else 'none')
     def check_str2(path):
       for s in path:
-        if 'atte' in s:# or 'up_blocks_3' in s or 'norm' in s or 'bias' in s or 'emb' in s.lower() or 'conv_in' in s or 'conv_out' in s:
+        if 'atte' not in s:# or 'up_blocks_3' in s or 'norm' in s or 'bias' in s or 'emb' in s.lower() or 'conv_in' in s or 'conv_out' in s:
             print("success ---> " , path )
             return True
 #       print("fail ----> ", path )      
