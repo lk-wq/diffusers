906bdf06f81e86393c13849d2e654d50bf120e68
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-07-20 18:29:11 -0500

    Update attention_flax.py

--- src/diffusers/models/attention_flax.py
+++ src/diffusers/models/attention_flax.py
@@ -158,7 +158,7 @@ class FlaxAttention(nn.Module):
         head_size = self.heads
         tensor = tensor.reshape(batch_size, seq_len, head_size, dim // head_size)
 
-        tensor = nn_partitioning.with_sharding_constraint(tensor, ("mp", None, "dp",None))
+        tensor = nn_partitioning.with_sharding_constraint(tensor, ("mp", None, None,'dp'))
         
         tensor = jnp.transpose(tensor, (0, 2, 1, 3))
         tensor = tensor.reshape(batch_size * head_size, seq_len, dim // head_size)
