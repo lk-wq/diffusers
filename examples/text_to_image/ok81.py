d7689a465f65687525df5fb5305f619c3e55202c
lk-wq <55562097+lk-wq@users.noreply.github.com>
2023-07-21 15:36:23 -0500

    Update and rename ok80.py to ok81.py

--- examples/text_to_image/ok81.py
+++ examples/text_to_image/ok81.py
@@ -1042,19 +1042,22 @@ def main():
     gc.collect()
 
     def partition_shape(shape):
-      # return P()
-
+      # for i in shape:
+      #   if 6 in shape:
+      #       if len(shape) == 1:
+      #           return P(None)
+      #       if len(shape) == 4:
+      #           return P(None,None,None,None)
       if len(shape) == 1:
         if shape[0] % 4 == 0:
           return P("dp")
         elif shape[0] % 2 == 0:
           return P("mp")
       if len(shape) == 2:
+        if shape[0] % 4 == 0 and shape[1] % 2 == 0 and shape[0] > shape[1]:
+          return P("dp","mp")
         if shape[0] % 2 == 0 and shape[1] % 4 == 0:
-          return P('mp','dp')
-        if shape[0] % 4 == 0 and shape[1] % 2 == 0:# and shape[0] >= shape[1]:
-          return P("dp",'mp')
-
+          return P("mp","dp")
         if shape[0] % 4 == 0:# and shape[1] % 2 == 0:
           return P("dp",None)
         if shape[1] % 4 == 0:# and shape[1] % 2 == 0:
@@ -1062,7 +1065,6 @@ def main():
         if shape[0] % 2 == 0 and shape[1] % 2 == 0:
           return P("mp",None)
       if len(shape) == 4:
-        print('shape',shape)
         if shape[-2] % 4 == 0 and shape[-1] % 2 == 0:
           return P(None,None,"dp","mp")
         if shape[-2] % 2 == 0 and shape[-1] % 4 == 0:
@@ -1074,7 +1076,7 @@ def main():
         if shape[-1] % 2 == 0 and shape[-2] % 2 == 0:
           return P(None,None,"mp",None)
         
-      print("fail")
+      print("fail",shape)
       return P()
     from jax.sharding import PartitionSpec as P 
     from jax.sharding import NamedSharding
